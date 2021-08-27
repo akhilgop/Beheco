5b5909fd602cafca5b4c1e407a682558d53e8983
akhilgop <akh_il@hotmail.com>
2021-08-27 11:36:50 +0530

    V1.1
    * Added sections till Freelancers

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,28 @@
 // ----------------------------------------------------------------nbbar On Scroll-------------------------------------------------------------------
 
-var nav = document.querySelector('nb'); // Identify target
+var nb = document.querySelector('.nb'); // Identify target
 window.addEventListener('scroll', function (event) { // To listen for event
     event.preventDefault();
 
     if (window.scrollY <= 150) {
-        nb.style.padding = '30px 0';
-        nb.style.backgroundColor = 'transparent';
+        nb.style.backgroundColor = 'white';
+        nb.style.boxShadow = '0px 1px 6px rgba(0, 0, 0, 0.1)';
     } else {
         nb.style.backgroundColor = '#fff';
         nb.style.boxShadow = '0px 1px 6px rgba(0, 0, 0, 0.1)';
-        nb.style.height = '80px';
         nb.style.padding = '0'
     }
 });
 
 
 
-var nav = document.querySelector('nb'); // Identify target
+var nb = document.querySelector('.nb'); // Identify target
 window.addEventListener('scroll', function (event) { // To listen for event
     event.preventDefault();
 
     if (window.scrollY == 0) {
         nb.style.boxShadow = 'none';
-        nb.style.height = '100px';
-        nb.style.backgroundColor = 'transparent';
+        nb.style.backgroundColor = 'white';
     }
 });
 
@@ -58,3 +56,65 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+// ----------------------------------------------------------------Video Slider-------------------------------------------------------------------
+// work in progress - needs some refactoring and will drop JQuery i promise :)
+var instance = $(".slider-wrapper");
+$.each(instance, function (key, value) {
+
+    var arrows = $(instance[key]).find(".arrow"),
+        prevArrow = arrows.filter('.arrow-prev'),
+        nextArrow = arrows.filter('.arrow-next'),
+        box = $(instance[key]).find(".hs"),
+        x = 0,
+        mx = 0,
+        maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+
+    $(arrows).on('click', function () {
+
+        if ($(this).hasClass("arrow-next")) {
+            x = ((box.width() / 2)) + box.scrollLeft() - 10;
+            box.animate({
+                scrollLeft: x,
+            })
+        } else {
+            x = ((box.width() / 2)) - box.scrollLeft() - 10;
+            box.animate({
+                scrollLeft: -x,
+            })
+        }
+
+    });
+
+    $(box).on({
+        mousemove: function (e) {
+            var mx2 = e.pageX - this.offsetLeft;
+            if (mx) this.scrollLeft = this.sx + mx - mx2;
+        },
+        mousedown: function (e) {
+            this.sx = this.scrollLeft;
+            mx = e.pageX - this.offsetLeft;
+        },
+        scroll: function () {
+            toggleArrows();
+        }
+    });
+
+    $(document).on("mouseup", function () {
+        mx = 0;
+    });
+
+    function toggleArrows() {
+        if (box.scrollLeft() > maxScrollWidth - 10) {
+            // disable next button when right end has reached 
+            nextArrow.addClass('disabled');
+        } else if (box.scrollLeft() < 10) {
+            // disable prev button when left end has reached 
+            prevArrow.addClass('disabled')
+        } else {
+            // both are enabled
+            nextArrow.removeClass('disabled');
+            prevArrow.removeClass('disabled');
+        }
+    }
+
+});
