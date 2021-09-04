3f6c4220b79932b6226603f57580f885e49f8036
akhilgop <akh_il@hotmail.com>
2021-09-04 12:46:39 +0530

    V1.6
    * Added slider for search results page
    * Search results page completed without responsive
    * Detail results page completed without responsive
    * Bug Fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -239,48 +239,152 @@ $(document).ready(function () {
 });
 
 // ----------------------------------------------------Custom File Upload-----------------------------------------------------//
-function GetFileInfo() {
-    var wrapper = $('<div/>').css({ height: 0, width: 0, 'overflow': 'hidden' });
-    var fileInput = document.getElementById("fileInput");
-    var message = "";
+$(document).ready(function () {
+    var $group = $('.input-group');
+    var $file = $group.find('input[type="file"]')
+    var $browse = $group.find('[data-action="browse"]');
+    var $fileDisplay = $group.find('[data-action="display"]');
+    var $reset = $group.find('[data-action="reset"]');
 
-    /*fileInput.change(function(){
-      $this = $(this);
-      $('#file').html($('input:file')[0].files.name);
-    });*/
-    if ('files' in fileInput) {
-        if (fileInput.files.length == 0) {
-            message = "Please browse for one or more files.";
-        } else {
-            for (var i = 0; i < fileInput.files.length; i++) {
-                var file = fileInput.files[i];
-                if ('name' in file) {
-                    message += file.name + "\t ";
-                }
-                else {
-                    message += file.fileName + "\t";
-                }
+
+    var resetHandler = function (e) {
+        if ($file.length === 0) {
+            return;
+        }
+
+        $file[0].value = '';
+        if (!/safari/i.test(navigator.userAgent)) {
+            $file[0].type = '';
+            $file[0].type = 'file';
+        }
+        $file.trigger('change');
+    };
+
+    var browseHandler = function (e) {
+        //If you select file A and before submitting you edit file A and reselect it it will not get the latest version, that is why we  might need to reset.
+        //resetHandler(e);
+        $file.trigger('click');
+
+    };
+
+    $browse.on('click', function (e) {
+        if (event.which != 1) {
+            return;
+        }
+        browseHandler();
+    });
+    $fileDisplay.on('click', function (e) {
+        if (event.which != 1) {
+            return;
+        }
+        browseHandler();
+    });
+    $reset.on('click', function (e) {
+        if (event.which != 1) {
+            return;
+        }
+
+        resetHandler();
+    });
+
+    $file.on('change', function (e) {
+        var files = [];
+        if (typeof e.currentTarget.files) {
+            for (var i = 0; i < e.currentTarget.files.length; i++) {
+                files.push(e.currentTarget.files[i].name.split('\\/').pop())
             }
+        } else {
+            files.push($(e.currentTarget).val().split('\\/').pop())
         }
-    }
-    else {
-        if (fileInput.value == "") {
-            message += "Please browse for one or more files.";
-            message += "<br />Use the Control or Shift key for multiple selection.";
-        }
-        else {
-            message += "Your browser doesn't support the files property!";
-            message += "<br />The path of the selected file: " + fileInput.value;
-        }
-    }
+        $fileDisplay.val(files.join('; '))
+    })
+});
 
-    var info = document.getElementById("file");
-    info.innerHTML = message;
-}
-/*$('#file').click(function(){
-    fileInput.click();
-}).show();*/
+// ----------------------------------------------------Gallery-----------------------------------------------------//
 
-$('.btn-upload').click(function () {
-    fileInput.click();
-}).show();
+// Gallery image hover
+$(".img-wrapper").hover(
+    function () {
+        $(this).find(".img-overlay").animate({ opacity: 1 }, 600);
+    }, function () {
+        $(this).find(".img-overlay").animate({ opacity: 0 }, 600);
+    }
+);
+
+// Lightbox
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $prevButton = $('<div id="prevButton"><i class="fa fa-chevron-left"></i></div>');
+var $nextButton = $('<div id="nextButton"><i class="fa fa-chevron-right"></i></div>');
+var $exitButton = $('<div id="exitButton"><i class="fa fa-times"></i></div>');
+
+// Add overlay
+$overlay.append($image).prepend($prevButton).append($nextButton).append($exitButton);
+$("#gallery").append($overlay);
+
+// Hide overlay on default
+$overlay.hide();
+
+// When an image is clicked
+$(".img-overlay").click(function (event) {
+    // Prevents default behavior
+    event.preventDefault();
+    // Adds href attribute to variable
+    var imageLocation = $(this).prev().attr("href");
+    // Add the image src to $image
+    $image.attr("src", imageLocation);
+    // Fade in the overlay
+    $overlay.fadeIn("slow");
+});
+
+// When the overlay is clicked
+$overlay.click(function () {
+    // Fade out the overlay
+    $(this).fadeOut("slow");
+});
+
+// When next button is clicked
+$nextButton.click(function (event) {
+    // Hide the current image
+    $("#overlay img").hide();
+    // Overlay image location
+    var $currentImgSrc = $("#overlay img").attr("src");
+    // Image with matching location of the overlay image
+    var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
+    // Finds the next image
+    var $nextImg = $($currentImg.closest(".image").next().find("img"));
+    // All of the images in the gallery
+    var $images = $("#image-gallery img");
+    // If there is a next image
+    if ($nextImg.length > 0) {
+        // Fade in the next image
+        $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
+    } else {
+        // Otherwise fade in the first image
+        $("#overlay img").attr("src", $($images[0]).attr("src")).fadeIn(800);
+    }
+    // Prevents overlay from being hidden
+    event.stopPropagation();
+});
+
+// When previous button is clicked
+$prevButton.click(function (event) {
+    // Hide the current image
+    $("#overlay img").hide();
+    // Overlay image location
+    var $currentImgSrc = $("#overlay img").attr("src");
+    // Image with matching location of the overlay image
+    var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
+    // Finds the next image
+    var $nextImg = $($currentImg.closest(".image").prev().find("img"));
+    // Fade in the next image
+    $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
+    // Prevents overlay from being hidden
+    event.stopPropagation();
+});
+
+// When the exit button is clicked
+$exitButton.click(function () {
+    // Fade out the overlay
+    $("#overlay").fadeOut("slow");
+});
